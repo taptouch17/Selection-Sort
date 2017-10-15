e1bdd112cac7fadaf1c4471fb7229e32222533c2
Graeme Stoute <graemeboy@hotmail.com>
2017-10-15 14:36:15 -0400

    Selection Sort

--- Selection Sort/main.swift
+++ Selection Sort/main.swift
@@ -10,3 +10,27 @@ import Foundation
 
 print("Hello, World!")
 
+func selectionSort(_ array: [Int]) -> [Int] {
+    guard array.count > 1 else { return array }  // 1
+    
+    var a = array                    // 2
+    
+    for x in 0 ..< a.count - 1 {     // 3
+        
+        var lowest = x
+        for y in x + 1 ..< a.count {   // 4
+            if a[y] < a[lowest] {
+                lowest = y
+            }
+        }
+        
+        if x != lowest {               // 5
+            a.swapAt(x, lowest)
+        }
+    }
+    return a
+}
+
+let array = [12,14,3,5,2,7,100,21]
+
+print(selectionSort(array))
