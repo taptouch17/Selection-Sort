951d4c30eae35e47a0ee9b05527cb9baaaac2eb5
Graeme Stoute <graemeboy@hotmail.com>
2017-10-15 14:01:31 -0400

    Initial Commit

---
+++ Selection Sort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Selection Sort
+//
+//  Created by Graeme Stoute on 15/10/2017.
+//  Copyright © 2017 Graeme Stoute. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
